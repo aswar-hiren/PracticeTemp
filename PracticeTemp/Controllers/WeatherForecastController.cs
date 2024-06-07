3949e24725557221ef111a962e142cc2db777350
harshtatvasoft <harsh.rathod@etatvasoft.com>
2024-06-07 15:02:32 +0530

    c

--- PracticeTemp/Controllers/WeatherForecastController.cs
+++ PracticeTemp/Controllers/WeatherForecastController.cs
@@ -21,6 +21,7 @@ namespace PracticeTemp.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
+            var harsh = "harsh";
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
